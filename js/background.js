051c1994e4fceabeaaf1399b7fe3794b7c2b07bc
Manraj Singh <manrajsinghgrover@gmail.com>
2016-07-16 21:49:01 +0530

    Few modifications

--- js/background.js
+++ js/background.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.method == "setStorage") {
-        localStorage.setItem("extdata", message.newData);
-    }
+  if (message.method == "setStorage") {
+    localStorage.setItem("extdata", message.newData);
+  }
 });
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.method == "setBookmark") {
-        localStorage.setItem("bookmark", message.newData);
-    }
+  if (message.method == "setBookmark") {
+    localStorage.setItem("bookmark", message.newData);
+  }
 });
