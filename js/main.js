a19273a2eb230faa8a5433d5ecb0b20ea7c247e6
Manraj Singh <manrajsinghgrover@gmail.com>
2016-07-26 12:51:13 +0530

    Formats code

--- js/main.js
+++ js/main.js
@@ -1,190 +1,189 @@
 function remove(link) {
-    var a = JSON.parse(localStorage.getItem('geekiT'));
-    if (a.filter(function(obj) {
-            return obj.url == link
-        }).length > 0) {
-        var removed = jQuery.grep(a, function(obj) {
-            return obj.url !== link;
-        });
-        localStorage.setItem('geekiT', JSON.stringify(removed));
-    }
-    chrome.runtime.sendMessage({
-        method: "setStorage",
-        newData: localStorage.getItem('geekiT')
+  var a = JSON.parse(localStorage.getItem('geekiT'));
+  if (a.filter(function(obj) {
+      return obj.url == link
+    }).length > 0) {
+    var removed = jQuery.grep(a, function(obj) {
+      return obj.url !== link;
     });
+    localStorage.setItem('geekiT', JSON.stringify(removed));
+  }
+  chrome.runtime.sendMessage({
+    method: "setStorage",
+    newData: localStorage.getItem('geekiT')
+  });
 }
 
 function add(link, title) {
-    var x = new Date();
-    var year = x.getFullYear();
-    var month = x.getMonth() + 1;
-    var date = x.getDate();
-    var time = year + '-' + month + '-' + date + ' 0:00PM';
-    title = (title == "" ? "GeeksForGeeks" : title);
-    var a = JSON.parse(localStorage.getItem('geekiT'));
-    if (a.filter(function(obj) {
-            return obj.url == link
-        }).length == 0) {
-        a.push({
-            url: link,
-            title: title,
-            stamp: time
-        });
-    }
-    localStorage.setItem('geekiT', JSON.stringify(a));
-    chrome.runtime.sendMessage({
-        method: "setStorage",
-        newData: localStorage.getItem('geekiT')
+  var x = new Date();
+  var year = x.getFullYear();
+  var month = x.getMonth() + 1;
+  var date = x.getDate();
+  var time = year + '-' + month + '-' + date + ' 0:00PM';
+  title = (title == "" ? "GeeksForGeeks" : title);
+  var a = JSON.parse(localStorage.getItem('geekiT'));
+  if (a.filter(function(obj) {
+      return obj.url == link
+    }).length == 0) {
+    a.push({
+      url: link,
+      title: title,
+      stamp: time
     });
+  }
+  localStorage.setItem('geekiT', JSON.stringify(a));
+  chrome.runtime.sendMessage({
+    method: "setStorage",
+    newData: localStorage.getItem('geekiT')
+  });
 }
 
 function removeBookmark(link) {
-    var a = JSON.parse(localStorage.getItem('bookmark'));
-    if (a.filter(function(obj) {
-            return obj.url == link
-        }).length > 0) {
-        var removed = jQuery.grep(a, function(obj) {
-            return obj.url !== link;
-        });
-        localStorage.setItem('bookmark', JSON.stringify(removed));
-    }
-    chrome.runtime.sendMessage({
-        method: "setBookmark",
-        newData: localStorage.getItem('bookmark')
+  var a = JSON.parse(localStorage.getItem('bookmark'));
+  if (a.filter(function(obj) {
+      return obj.url == link
+    }).length > 0) {
+    var removed = jQuery.grep(a, function(obj) {
+      return obj.url !== link;
     });
+    localStorage.setItem('bookmark', JSON.stringify(removed));
+  }
+  chrome.runtime.sendMessage({
+    method: "setBookmark",
+    newData: localStorage.getItem('bookmark')
+  });
 }
 
 function addBookmark(link, title) {
-    var x = new Date();
-    var year = x.getFullYear();
-    var month = x.getMonth() + 1;
-    var date = x.getDate();
-    var time = year + '-' + month + '-' + date + ' 0:00PM';
-    title = (title == "" ? "GeeksForGeeks" : title);
-    var a = JSON.parse(localStorage.getItem('bookmark'));
-    if (a.filter(function(obj) {
-            return obj.url == link
-        }).length == 0) {
-        a.push({
-            url: link,
-            title: title,
-            stamp: time
-        });
-    }
-    localStorage.setItem('bookmark', JSON.stringify(a));
-    chrome.runtime.sendMessage({
-        method: "setBookmark",
-        newData: localStorage.getItem('bookmark')
+  var x = new Date();
+  var year = x.getFullYear();
+  var month = x.getMonth() + 1;
+  var date = x.getDate();
+  var time = year + '-' + month + '-' + date + ' 0:00PM';
+  title = (title == "" ? "GeeksForGeeks" : title);
+  var a = JSON.parse(localStorage.getItem('bookmark'));
+  if (a.filter(function(obj) {
+      return obj.url == link
+    }).length == 0) {
+    a.push({
+      url: link,
+      title: title,
+      stamp: time
     });
+  }
+  localStorage.setItem('bookmark', JSON.stringify(a));
+  chrome.runtime.sendMessage({
+    method: "setBookmark",
+    newData: localStorage.getItem('bookmark')
+  });
 }
 
 function bookmarkHelper() {
-    if ($('#bk').prop('checked')) {
-        addBookmark(document.URL, $(document).find("title").text());
-    } else {
-        removeBookmark(document.URL);
-    }
+  if ($('#bk').prop('checked')) {
+    addBookmark(document.URL, $(document).find("title").text());
+  } else {
+    removeBookmark(document.URL);
+  }
 }
 
 function check() {
-    var topFixed = $('#topfixed');
-    if ($('#geekiT').prop('checked')) {
-        topFixed.removeClass('notdone').addClass('done').text('DONE');
-        add(document.URL, $(document).find("title").text());
-    } else {
-        topFixed.removeClass('done').addClass('notdone').text('NOT DONE');
-        remove(document.URL);
-    }
+  var topFixed = $('#topfixed');
+  if ($('#geekiT').prop('checked')) {
+    topFixed.removeClass('notdone').addClass('done').text('DONE');
+    add(document.URL, $(document).find("title").text());
+  } else {
+    topFixed.removeClass('done').addClass('notdone').text('NOT DONE');
+    remove(document.URL);
+  }
 }
 
 
 function cutLinks() {
-    var a = JSON.parse(localStorage["geekiT"]);
-    $(".site-content a,#post a").each(function() {
-        var link = this.href;
-        if (a.filter(function(obj) {
-                return obj.url == link
-            }).length > 0) {
-            $(this).css({
-                "color": "#006600",
-                "text-decoration": "line-through"
-            });
-        }
-    });
+  var a = JSON.parse(localStorage["geekiT"]);
+  $(".site-content a,#post a").each(function() {
+    var link = this.href;
+    if (a.filter(function(obj) {
+        return obj.url == link
+      }).length > 0) {
+      $(this).css({
+        "color": "#006600",
+        "text-decoration": "line-through"
+      });
+    }
+  });
 }
 
 function bookmarkLinks() {
-    var a = JSON.parse(localStorage["bookmark"]);
-    $(".site-content a,#post a").each(function() {
-        var link = this.href;
-        if (a.filter(function(obj) {
-                return obj.url == link
-            }).length > 0) {
-            $(this).css({
-                "color": "#CC0000",
-                "font-weight": "bold"
-            });
-        }
-    });
+  var a = JSON.parse(localStorage["bookmark"]);
+  $(".site-content a,#post a").each(function() {
+    var link = this.href;
+    if (a.filter(function(obj) {
+        return obj.url == link
+      }).length > 0) {
+      $(this).css({
+        "color": "#CC0000",
+        "font-weight": "bold"
+      });
+    }
+  });
 }
 
 $(document).ready(function() {
+  if (typeof localStorage['geekiT'] == 'undefined') {
+    localStorage.setItem('geekiT', JSON.stringify([]));
+  }
+  if (typeof localStorage['bookmark'] == 'undefined') {
+    localStorage.setItem('bookmark', JSON.stringify([]));
+  }
 
-    if (typeof localStorage['geekiT'] == 'undefined') {
-        localStorage.setItem('geekiT', JSON.stringify([]));
+  var a = JSON.parse(localStorage.getItem('geekiT'));
+  var send = localStorage.getItem('geekiT');
+  var link = document.URL;
+  var pat1 = "http://www.geeksforgeeks.org/";
+  var pat2 = "http://www.geeksforgeeks.org/page/";
+  var pat3 = "https://www.geeksforgeeks.org/";
+  var pat4 = "https://www.geeksforgeeks.org/page/";
+
+  if (!(link == pat1) && !(link == pat3) && !(link.search(pat2) >= 0) && !(link.search(pat4) >= 0)) {
+    if (a.filter(function(obj) {
+        return obj.url == link
+      }).length > 0) {
+      var geeked = '<div id="topfixed" class="done">DONE</div>';
+      var div = "<div id='ui'>geek<span style='color:red;'>iT</span>? <input id='geekiT' name='geekiT' type='checkbox' checked='true' /></div>";
+    } else {
+      var geeked = '<div id="topfixed" class="notdone">NOT DONE</div>';
+      var div = "<div id='ui'>geek<span style='color:red;'>iT</span>? <input id='geekiT' name='geekiT' type='checkbox'/></div>";
     }
-    if (typeof localStorage['bookmark'] == 'undefined') {
-        localStorage.setItem('bookmark', JSON.stringify([]));
-    }
-
-    var a = JSON.parse(localStorage.getItem('geekiT'));
-    var send = localStorage.getItem('geekiT');
-    var link = document.URL;
-    var pat1 = "http://www.geeksforgeeks.org/";
-    var pat2 = "http://www.geeksforgeeks.org/page/";
-    var pat3 = "https://www.geeksforgeeks.org/";
-    var pat4 = "https://www.geeksforgeeks.org/page/";
-
-    if (!(link == pat1) && !(link == pat3) && !(link.search(pat2) >= 0) && !(link.search(pat4) >= 0)) {
-        if (a.filter(function(obj) {
-                return obj.url == link
-            }).length > 0) {
-            var geeked = '<div id="topfixed" class="done">DONE</div>';
-            var div = "<div id='ui'>geek<span style='color:red;'>iT</span>? <input id='geekiT' name='geekiT' type='checkbox' checked='true' /></div>";
-        } else {
-            var geeked = '<div id="topfixed" class="notdone">NOT DONE</div>';
-            var div = "<div id='ui'>geek<span style='color:red;'>iT</span>? <input id='geekiT' name='geekiT' type='checkbox'/></div>";
-        }
-        var a = JSON.parse(localStorage.getItem('bookmark'));
-        var bookmark = localStorage.getItem('bookmark');
-        var link = document.URL;
-        if (a.filter(function(obj) {
-                return obj.url == link
-            }).length > 0) {
-            var bk = "<div id='ui'>bookmark<span style='color:red;'>iT</span>? <input id='bk' name='bk' type='checkbox' checked='true' /></div>";
-        } else {
-            var bk = "<div id='ui'>bookmark<span style='color:red;'>iT</span>? <input id='bk' name='bk' type='checkbox'/></div>";
-        }
-
-        $("body").append(geeked).append("<div id='geekbox'></div>");
-        $("#geekbox").append(div).append(bk).show();
-        $("#bk").show();
-    }
-    cutLinks();
-    bookmarkLinks();
-    chrome.runtime.sendMessage({
-        method: "setStorage",
-        newData: send
-    });
+    var a = JSON.parse(localStorage.getItem('bookmark'));
     var bookmark = localStorage.getItem('bookmark');
-    chrome.runtime.sendMessage({
-        method: "setBookmark",
-        newData: bookmark
-    });
+    var link = document.URL;
+    if (a.filter(function(obj) {
+        return obj.url == link
+      }).length > 0) {
+      var bk = "<div id='ui'>bookmark<span style='color:red;'>iT</span>? <input id='bk' name='bk' type='checkbox' checked='true' /></div>";
+    } else {
+      var bk = "<div id='ui'>bookmark<span style='color:red;'>iT</span>? <input id='bk' name='bk' type='checkbox'/></div>";
+    }
+
+    $("body").append(geeked).append("<div id='geekbox'></div>");
+    $("#geekbox").append(div).append(bk).show();
+    $("#bk").show();
+  }
+  cutLinks();
+  bookmarkLinks();
+  chrome.runtime.sendMessage({
+    method: "setStorage",
+    newData: send
+  });
+  var bookmark = localStorage.getItem('bookmark');
+  chrome.runtime.sendMessage({
+    method: "setBookmark",
+    newData: bookmark
+  });
 });
 $(document).on('click', '#geekiT', function() {
-    check();
+  check();
 });
 $(document).on('click', '#bk', function() {
-    bookmarkHelper();
+  bookmarkHelper();
 });
--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,9 @@
 /*
-* @Author: Manraj Singh
-* @Date:   2016-03-25 18:37:53
-* @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-03-25 18:41:56
-*/
+ * @Author: Manraj Singh
+ * @Date:   2016-03-25 18:37:53
+ * @Last Modified by:   Manraj Singh
+ * @Last Modified time: 2016-07-26 12:50:19
+ */
 
 /**
  * [parseDate returns date in UTC format]
@@ -13,118 +13,118 @@
 function parseDate(input) {
   var dateAndTime = input.split(' ');
   var parts = dateAndTime[0].split('-');
-  return Date.UTC(parts[0], parts[1]-1, parts[2]);
+  return Date.UTC(parts[0], parts[1] - 1, parts[2]);
 }
 
 /**
  * Initializes popup on browser popup click.
  */
 $(document).ready(function() {
-    $("#links").css("color", "black");
-    $("#analyze, #bookmark").css("color", "lightgrey");
-    $("#analytics, #bookmarked").hide();
-    if (typeof localStorage['extdata'] == 'undefined') {
-        var list = [];
-        localStorage.setItem('extdata', JSON.stringify(list));
-    }
-    var a = JSON.parse(localStorage.getItem('extdata'));
-    var it = 0;
-    var innerhtml = "<table class='table'><thead><tr><th>#</th><th>Title</th><th>Date</th></tr></thead><tbody>";
-    for (var i = a.length - 1; i >= 0; i--) {
-        var time = a[i]['stamp'];
-        time = time.substring(0, time.length - 7).split("-");
-        var final_time = time[2]+"-"+time[1]+"-"+time[0];
-        it++;
-        innerhtml += "<tr><td>" + it + "</td><td><a href='" + a[i]['url'] + "' target='_blank'>" + a[i]['title'] + "</a></td><td style='width:150px;'>" + final_time + "</td></tr>";
-    }
-    innerhtml += "</tbody></table>";
-    $("#content").html(innerhtml);
-    if (typeof localStorage['bookmark'] == 'undefined') {
-        var list = [];
-        localStorage.setItem('bookmark', JSON.stringify(list));
-    }
-    var a = JSON.parse(localStorage.getItem('bookmark'));
-    it = 0;
-    var innerhtml = "<table class='table'><thead><tr><th>#</th><th>Title</th><th>Date</th></tr></thead><tbody>";
-    for (var i = a.length - 1; i >= 0; i--) {
-        var time = a[i]['stamp'];
-        time = time.substring(0, time.length - 7).split("-");
-        var final_time = time[2]+"-"+time[1]+"-"+time[0];
-        it++;
-        innerhtml += "<tr><td>" + it + "</td><td><a href='" + a[i]['url'] + "' target='_blank'>" + a[i]['title'] + "</a></td><td style='width:150px;'>" + final_time + "</td></tr>";
-    }
-    innerhtml += "</tbody></table>";
-    $("#bookmarked").html(innerhtml);
+  $("#links").css("color", "black");
+  $("#analyze, #bookmark").css("color", "lightgrey");
+  $("#analytics, #bookmarked").hide();
+  if (typeof localStorage['extdata'] == 'undefined') {
+    var list = [];
+    localStorage.setItem('extdata', JSON.stringify(list));
+  }
+  var a = JSON.parse(localStorage.getItem('extdata'));
+  var it = 0;
+  var innerhtml = "<table class='table'><thead><tr><th>#</th><th>Title</th><th>Date</th></tr></thead><tbody>";
+  for (var i = a.length - 1; i >= 0; i--) {
+    var time = a[i]['stamp'];
+    time = time.substring(0, time.length - 7).split("-");
+    var final_time = time[2] + "-" + time[1] + "-" + time[0];
+    it++;
+    innerhtml += "<tr><td>" + it + "</td><td><a href='" + a[i]['url'] + "' target='_blank'>" + a[i]['title'] + "</a></td><td style='width:150px;'>" + final_time + "</td></tr>";
+  }
+  innerhtml += "</tbody></table>";
+  $("#content").html(innerhtml);
+  if (typeof localStorage['bookmark'] == 'undefined') {
+    var list = [];
+    localStorage.setItem('bookmark', JSON.stringify(list));
+  }
+  var a = JSON.parse(localStorage.getItem('bookmark'));
+  it = 0;
+  var innerhtml = "<table class='table'><thead><tr><th>#</th><th>Title</th><th>Date</th></tr></thead><tbody>";
+  for (var i = a.length - 1; i >= 0; i--) {
+    var time = a[i]['stamp'];
+    time = time.substring(0, time.length - 7).split("-");
+    var final_time = time[2] + "-" + time[1] + "-" + time[0];
+    it++;
+    innerhtml += "<tr><td>" + it + "</td><td><a href='" + a[i]['url'] + "' target='_blank'>" + a[i]['title'] + "</a></td><td style='width:150px;'>" + final_time + "</td></tr>";
+  }
+  innerhtml += "</tbody></table>";
+  $("#bookmarked").html(innerhtml);
 });
 
 /**
  * Event listeners for all tabs and manipulations
  */
 $(document).on("click", "#links", function() {
-    $("#links").css("color", "black");
-    $("#analyze, #bookmark").css("color", "lightgrey");
-    $("#analytics, #bookmarked").hide();
-    $("#content").show();
+  $("#links").css("color", "black");
+  $("#analyze, #bookmark").css("color", "lightgrey");
+  $("#analytics, #bookmarked").hide();
+  $("#content").show();
 });
 
 $(document).on("click", "#bookmark", function() {
-    $("#links, #analyze").css("color", "lightgrey");
-    $("#bookmark").css("color", "black");
-    $("#analytics, #content").hide();
-    $("#bookmarked").show();
+  $("#links, #analyze").css("color", "lightgrey");
+  $("#bookmark").css("color", "black");
+  $("#analytics, #content").hide();
+  $("#bookmarked").show();
 });
 $(document).on("click", "#analyze", function() {
-    $("#analyze").css("color", "black");
-    $("#links, #bookmark").css("color", "lightgrey");
-    $("#content, #bookmarked").hide();
-    $("#analytics").show();
-    var a = JSON.parse(localStorage.getItem('extdata'));
-    var count = {};
-    for (var i = 0; i < a.length; i++) {
-        var s = a[i]['stamp'];
-        if (!count.hasOwnProperty(a[i]['stamp'])) {
-            count[s] = 1;
-        } else {
-            count[s] += 1;
-        }
+  $("#analyze").css("color", "black");
+  $("#links, #bookmark").css("color", "lightgrey");
+  $("#content, #bookmarked").hide();
+  $("#analytics").show();
+  var a = JSON.parse(localStorage.getItem('extdata'));
+  var count = {};
+  for (var i = 0; i < a.length; i++) {
+    var s = a[i]['stamp'];
+    if (!count.hasOwnProperty(a[i]['stamp'])) {
+      count[s] = 1;
+    } else {
+      count[s] += 1;
     }
-    var arr = [];
-    for (var prop in count) {
-        arr.push([parseDate(prop), count[prop]]);
-    }
-    $('#analytics').highcharts({
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Your Progress',
-            x: -20
-        },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: 'Questions solved'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: ' Questions'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Progress',
-            data: arr
-        }]
-    });
+  }
+  var arr = [];
+  for (var prop in count) {
+    arr.push([parseDate(prop), count[prop]]);
+  }
+  $('#analytics').highcharts({
+    chart: {
+      zoomType: 'x'
+    },
+    title: {
+      text: 'Your Progress',
+      x: -20
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: {
+      title: {
+        text: 'Questions solved'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: ' Questions'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'Progress',
+      data: arr
+    }]
+  });
 });
